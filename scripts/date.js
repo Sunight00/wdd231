1a7afe45ac04199644dabf48c52cdce107ed57ff
Sunight00 <ephraim.ewane@gmail.com>
2025-03-03 21:40:35 +0100

    going

--- scripts/date.js
+++ scripts/date.js
@@ -4,3 +4,10 @@ const today = new Date();
 
 
 year.innerHTML = today.getFullYear();
+
+
+
+
+const mod = document.querySelector("#lastmod");
+let oLastModif = new Date(document.lastModified);
+mod.innerHTML=oLastModif;
