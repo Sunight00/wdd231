3d01c818df99a19600b85c6c4999c5fec6c7bb83
Sunight00 <ephraim.ewane@gmail.com>
2025-03-12 20:37:58 +0100

    hgoing

--- scripts/prophets.js
+++ scripts/prophets.js
@@ -1,5 +1,5 @@
 const url = "https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json";
-//const cards = document.querySelector('#cards');
+const cards = document.querySelector('#cards');
 
 async function getProphetData(){
     let response = await fetch(url);
@@ -13,7 +13,24 @@ async function getProphetData(){
 
 const displayProphets = (prophets) => 
     {
-        
+        prophets.forEach(prophet => {
+            let card = document.createElement("section");
+            let fullName = document.createElement("h2");
+            let portrait = document.createElement("img");
+            
+            fullName.innerHTML = `${prophet.name} ${prophet.lastname}`;
+            portrait.setAttribute('src', prophet.imageurl);
+            portrait.setAttribute('alt', `${prophet.name} ${prophet.lastname}`);
+            portrait.setAttribute('loading', 'lazy');
+            portrait.setAttribute('width', '200');
+            portrait.setAttribute('height', '200');
+
+            card.appendChild(fullName);
+            card.appendChild(portrait);
+
+            cards.appendChild(card);
+
+        });
     }
 
 getProphetData();
