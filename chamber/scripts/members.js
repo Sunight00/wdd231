1870e2b3b2f8d5db276bce41207e26bf46b9dc0a
Sunight00 <ephraim.ewane@gmail.com>
2025-03-14 09:03:47 +0100

    going

---
+++ chamber/scripts/members.js
@@ -0,0 +1,42 @@
+const url = "https://sunight00.github.io/wdd231/chamber/data/members.json"
+
+async function  getMembers() {
+    let response = await fetch(url);
+    let data = await response.json();
+    
+    displayMembers(data.companies);   
+}
+
+ // Promise {<pending>}
+
+
+ function displayMembers(members) {
+    members.forEach(member => {
+       
+        let card = document.createElement("section");
+        let name = document.createElement("h2");
+        let address = document.createElement("p");
+        let phone = document.createElement("p");
+        let website = document.createElement("p");
+        let logo = document.createElement("img");
+
+        logo.setAttribute("src", member.image);
+
+        name.innerHTML = member.name;
+        address.innerHTML = member.address;
+        phone.innerHTML = member.phone;
+        website.innerHTML = member.website;
+
+        card.appendChild(logo);
+        card.appendChild(name);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(website);
+
+        const n = document.querySelector("#members");
+        n.appendChild(card);
+
+    });
+ }
+
+ getMembers();
