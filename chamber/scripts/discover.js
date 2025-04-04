94eed393516189dfbd0d8673353bc9e30e5e185e
Sunight00 <ephraim.ewane@gmail.com>
2025-04-04 10:40:56 +0100

    sss

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -24,7 +24,7 @@ function discover(goo){
         description.setAttribute("class", "description");
         let address = document.createElement("p");
         address.setAttribute("class", "address");
-        let button = document.createElement("a");
+        let button = document.createElement("button");
 
         title.innerHTML = info.title;
         img.setAttribute("src", info.image);
@@ -35,7 +35,7 @@ function discover(goo){
         address.innerHTML = info.address;
 
         button.innerHTML = "Learn More";
-        button.setAttribute("href", "#");
+        //button.setAttribute("href", "#");
 
         card.appendChild(title);
         card.appendChild(img);
@@ -46,3 +46,33 @@ function discover(goo){
         document.querySelector("#discoverymain").appendChild(card);
     });
 }
+
+window.addEventListener("load", function() {
+    const date = localStorage.getItem("date");
+    window.alert(`Last session ${date}`);
+    const today = new Date();
+
+    const day = today.getDay()+1;
+    const month = today.getMonth()+1;
+    const year = today.getFullYear();
+    const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    const Datee= `${day}/${month}/${year} : ${time}`;
+    localStorage.setItem("date", Datee);
+    //document.querySelector("#aa").innerHTML = date;
+})
+
+/*
+function date(){
+const today = new Date();
+
+const day = today.getDay()+1;
+const month = today.getMonth()+1;
+const year = today.getFullYear();
+const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+const Datee= `${day}/${month}/${year}  ${time}`;
+localStorage.setItem("date", Datee);
+}*/
+
+
+
+
