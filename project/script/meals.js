192106dfc1f3b32e66719e862991dc038263c921
Sunight00 <ephraim.ewane@gmail.com>
2025-04-10 17:05:06 +0100

    nn

---
+++ project/script/meals.js
@@ -0,0 +1,33 @@
+const url = ""
+
+function createForeign(dish)
+{ console.log(dish)
+    dish.forEach(dishes => {
+       
+    /*let card = document.createElement('section');
+    let name = document.createElement('h2');
+    let origin = document.createElement('p');
+    let recipe = document.createElement('p');
+    let health_benefits = document.createElement('p');
+    let image = document.createElement('img');
+
+
+    name.innerHTML = dishes.name;
+    origin.innerHTML = dishes.origin;
+    recipe.innerHTML = dishes.recipe;
+    health_benefits.innerHTML = dishes.health_benefits;
+    image.setAttribute('src', dishes.image);
+    image.setAttribute('alt', dishes.name);
+    image.setAttribute('width', '350px');
+    image.setAttribute('height', '300px');
+
+    card.appendChild(name);
+    card.appendChild(origin);
+    card.appendChild(recipe);
+    card.appendChild(health_benefits);
+    card.appendChild(image);
+
+    document.querySelector('.foreign').appendChild(card);*/
+    });
+    
+}
