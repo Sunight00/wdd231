e814090469853c515be1d6c550851b5122fc637f
Sunight00 <ephraim.ewane@gmail.com>
2025-04-12 23:24:50 +0100

    hh

--- project/script/modal.js
+++ project/script/modal.js
@@ -1,6 +1,119 @@
-const shownp = document.querySelector("#dish1");
-//const closenp = document.querySelector("#closenp");
-const np= document.querySelector("#open1");
-shownp.addEventListener("click",()=>{
-    np.showModal()
+const opensushi = document.querySelector("#opensushi");
+const modalsushi = document.querySelector("#modalsushi");
+const closesushi = document.querySelector("#closesushi");
+
+opensushi.addEventListener("click",()=>{
+    modalsushi.showModal()
 })
+closesushi.addEventListener("click",()=>{
+    modalsushi.close()
+});
+
+
+const opentacos = document.querySelector("#opentacos");
+const modaltacos = document.querySelector("#modaltacos");
+const closetacos = document.querySelector("#closetacos");
+
+opentacos.addEventListener("click",()=>{
+    modaltacos.showModal()
+})
+closetacos.addEventListener("click",()=>{
+    modaltacos.close()
+})
+
+
+const openrata = document.querySelector("#openrata");
+const modalrata = document.querySelector("#modalrata");
+const closerata = document.querySelector("#closerata");
+
+openrata.addEventListener("click",()=>{
+    modalrata.showModal()
+})
+closerata.addEventListener("click",()=>{
+    modalrata.close()
+})
+
+
+const openbibi = document.querySelector("#openbibi");
+const modalbibi = document.querySelector("#modalbibi");
+const closebibi = document.querySelector("#closebibi");
+
+openbibi.addEventListener("click",()=>{
+    modalbibi.showModal()
+})
+closebibi.addEventListener("click",()=>{
+    modalbibi.close()
+})
+
+
+
+const openjellof = document.querySelector("#openjellof");
+const modaljellof = document.querySelector("#modaljellof");
+const closejellof = document.querySelector("#closejellof");
+
+openjellof.addEventListener("click",()=>{
+    modaljellof.showModal()
+})
+closejellof.addEventListener("click",()=>{
+    modaljellof.close()
+});
+
+
+const openegusi = document.querySelector("#openegusi");
+const modalegusi = document.querySelector("#modalegusi");
+const closeegusi = document.querySelector("#closeegusi");
+
+openegusi.addEventListener("click",()=>{
+    modalegusi.showModal()
+})
+closeegusi.addEventListener("click",()=>{
+    modalegusi.close()
+});
+
+
+const openbunny = document.querySelector("#openbunny");
+const modalbunny = document.querySelector("#modalbunny");
+const closebunny = document.querySelector("#closebunny");
+
+openbunny.addEventListener("click",()=>{
+    modalbunny.showModal()
+})
+closebunny.addEventListener("click",()=>{
+    modalbunny.close()
+});
+
+
+const opensuya = document.querySelector("#opensuya");
+const modalsuya = document.querySelector("#modalsuya");
+const closesuya = document.querySelector("#closesuya");
+
+opensuya.addEventListener("click",()=>{
+    modalsuya.showModal()
+})
+closesuya.addEventListener("click",()=>{
+    modalsuya.close()
+});
+
+
+
+const openmatoke = document.querySelector("#openmatoke");
+const modalmatoke = document.querySelector("#modalmatoke");
+const closematoke = document.querySelector("#closematoke");
+
+openmatoke.addEventListener("click",()=>{
+    modalmatoke.showModal()
+})
+closematoke.addEventListener("click",()=>{
+    modalmatoke.close()
+});
+
+const openkoshari = document.querySelector("#openkoshari");
+const modalkoshari = document.querySelector("#modalkoshari");
+const closekoshari = document.querySelector("#closekoshari");
+
+openkoshari.addEventListener("click",()=>{
+    modalkoshari.showModal()
+})
+closekoshari.addEventListener("click",()=>{
+    modalkoshari.close()
+});
