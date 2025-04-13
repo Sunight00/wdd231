2ec4e3bd268d50462a1a8db306921944b3d40284
Sunight00 <ephraim.ewane@gmail.com>
2025-04-13 20:07:22 +0100

    okay

--- project/script/localstorage.js
+++ project/script/localstorage.js
@@ -1,15 +1,244 @@
-const sub = document.querySelector('#subtac');
+sushi()
+function sushi(){
+    const sub = document.querySelector('#subsushi');
 sub.addEventListener('click',()=>{
-    const date = Date.now();
-    const input = document.querySelector('#input').value;
-   const n= [1,2,3,];
-    n.push(input);
-    localStorage.setItem('list', n);
-})
+    const today = new Date();
 
-const su = document.querySelector('#subta');
-su.addEventListener('click',()=>{
-    const inpt = document.querySelector('#inpt').value;
-   
-    localStorage.setItem('lis', inpt);
+    const day = today.getDay()+1;
+    const month = today.getMonth()+1;
+    const year = today.getFullYear();
+    const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    const Datee= `${day}/${month}/${year} : ${time}`;
+
+
+    const name = document.querySelector('#sushiname').value;
+    const num = document.querySelector('#sushiqty').value;
+    const add = document.querySelector('#sushiadd').value;
+    const phone = document.querySelector('#sushiphone').value;
+    const info = `DATE-TIME:${Datee}/ NAME:${name}/ QTY:${num}/ ADDRESS:${add}/ TEL:${phone}`;
+    localStorage.setItem("order",info);
 })
+}
+
+
+
+tacos()
+function tacos(){
+    const sub = document.querySelector('#subtaco');
+sub.addEventListener('click',()=>{
+    const today = new Date();
+
+    const day = today.getDay()+1;
+    const month = today.getMonth()+1;
+    const year = today.getFullYear();
+    const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    const Datee= `${day}/${month}/${year} : ${time}`;
+
+
+    const name = document.querySelector('#taconame').value;
+    const num = document.querySelector('#tacoqty').value;
+    const add = document.querySelector('#tacoadd').value;
+    const phone = document.querySelector('#tacophone').value;
+    const info = `DATE-TIME:${Datee}/ NAME:${name}/ QTY:${num}/ ADDRESS:${add}/ TEL:${phone}`;
+    localStorage.setItem("order",info);
+})
+}
+
+
+
+rata()
+function rata(){
+    const sub = document.querySelector('#subrata');
+sub.addEventListener('click',()=>{
+    const today = new Date();
+
+    const day = today.getDay()+1;
+    const month = today.getMonth()+1;
+    const year = today.getFullYear();
+    const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    const Datee= `${day}/${month}/${year} : ${time}`;
+
+
+    const name = document.querySelector('#rataname').value;
+    const num = document.querySelector('#rataqty').value;
+    const add = document.querySelector('#rataadd').value;
+    const phone = document.querySelector('#rataphone').value;
+    const info = `DATE-TIME:${Datee}/ NAME:${name}/ QTY:${num}/ ADDRESS:${add}/ TEL:${phone}`;
+    localStorage.setItem("order",info);
+})
+}
+
+
+bibi()
+function bibi(){
+    const sub = document.querySelector('#bibisub');
+sub.addEventListener('click',()=>{
+    const today = new Date();
+
+    const day = today.getDay()+1;
+    const month = today.getMonth()+1;
+    const year = today.getFullYear();
+    const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    const Datee= `${day}/${month}/${year} : ${time}`;
+
+
+    const name = document.querySelector('#bibiname').value;
+    const num = document.querySelector('#bibiqty').value;
+    const add = document.querySelector('#bibiadd').value;
+    const phone = document.querySelector('#bibiphone').value;
+    const info = `DATE-TIME:${Datee}/ NAME:${name}/ QTY:${num}/ ADDRESS:${add}/ TEL:${phone}`;
+    localStorage.setItem("order",info);
+})
+}
+
+
+
+jell()
+function jell(){
+    const sub = document.querySelector('#jellsub');
+sub.addEventListener('click',()=>{
+    const today = new Date();
+
+    const day = today.getDay()+1;
+    const month = today.getMonth()+1;
+    const year = today.getFullYear();
+    const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    const Datee= `${day}/${month}/${year} : ${time}`;
+
+
+    const name = document.querySelector('#jellname').value;
+    const num = document.querySelector('#jellqty').value;
+    const add = document.querySelector('#jelladd').value;
+    const phone = document.querySelector('#jellphone').value;
+    const info = `DATE-TIME:${Datee}/ NAME:${name}/ QTY:${num}/ ADDRESS:${add}/ TEL:${phone}`;
+    localStorage.setItem("order",info);
+})
+}
+
+
+
+
+egusi()
+function egusi(){
+    const sub = document.querySelector('#egusisub');
+sub.addEventListener('click',()=>{
+    const today = new Date();
+
+    const day = today.getDay()+1;
+    const month = today.getMonth()+1;
+    const year = today.getFullYear();
+    const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    const Datee= `${day}/${month}/${year} : ${time}`;
+
+
+    const name = document.querySelector('#egusiname').value;
+    const num = document.querySelector('#egusiqty').value;
+    const add = document.querySelector('#egusiadd').value;
+    const phone = document.querySelector('#egusiphone').value;
+    const info = `DATE-TIME:${Datee}/ NAME:${name}/ QTY:${num}/ ADDRESS:${add}/ TEL:${phone}`;
+    localStorage.setItem("order",info);
+})
+}
+
+
+
+bunny()
+function bunny(){
+    const sub = document.querySelector('#bunnysub');
+sub.addEventListener('click',()=>{
+    const today = new Date();
+
+    const day = today.getDay()+1;
+    const month = today.getMonth()+1;
+    const year = today.getFullYear();
+    const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    const Datee= `${day}/${month}/${year} : ${time}`;
+
+
+    const name = document.querySelector('#bunnyname').value;
+    const num = document.querySelector('#bunnyqty').value;
+    const add = document.querySelector('#bunnyadd').value;
+    const phone = document.querySelector('#bunnyphone').value;
+    const info = `DATE-TIME:${Datee}/ NAME:${name}/ QTY:${num}/ ADDRESS:${add}/ TEL:${phone}`;
+    localStorage.setItem("order",info);
+})
+}
+
+suya()
+function suya(){
+    const sub = document.querySelector('#suyasub');
+sub.addEventListener('click',()=>{
+    const today = new Date();
+
+    const day = today.getDay()+1;
+    const month = today.getMonth()+1;
+    const year = today.getFullYear();
+    const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    const Datee= `${day}/${month}/${year} : ${time}`;
+
+
+    const name = document.querySelector('#suyaname').value;
+    const num = document.querySelector('#suyaqty').value;
+    const add = document.querySelector('#suyaadd').value;
+    const phone = document.querySelector('#suyaphone').value;
+    const info = `DATE-TIME:${Datee}/ NAME:${name}/ QTY:${num}/ ADDRESS:${add}/ TEL:${phone}`;
+    localStorage.setItem("order",info);
+})
+}
+
+
+mato()
+function mato(){
+    const sub = document.querySelector('#matosub');
+sub.addEventListener('click',()=>{
+    const today = new Date();
+
+    const day = today.getDay()+1;
+    const month = today.getMonth()+1;
+    const year = today.getFullYear();
+    const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    const Datee= `${day}/${month}/${year} : ${time}`;
+
+
+    const name = document.querySelector('#matoname').value;
+    const num = document.querySelector('#matoqty').value;
+    const add = document.querySelector('#matoadd').value;
+    const phone = document.querySelector('#matophone').value;
+    const info = `DATE-TIME:${Datee}/ NAME:${name}/ QTY:${num}/ ADDRESS:${add}/ TEL:${phone}`;
+    localStorage.setItem("order",info);
+})
+}
+
+kosh()
+function kosh(){
+    const sub = document.querySelector('#koshsub');
+sub.addEventListener('click',()=>{
+    const today = new Date();
+
+    const day = today.getDay()+1;
+    const month = today.getMonth()+1;
+    const year = today.getFullYear();
+    const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    const Datee= `${day}/${month}/${year} : ${time}`;
+
+
+    const name = document.querySelector('#koshname').value;
+    const num = document.querySelector('#koshqty').value;
+    const add = document.querySelector('#koshadd').value;
+    const phone = document.querySelector('#koshphone').value;
+    const info = `DATE-TIME:${Datee}/ NAME:${name}/ QTY:${num}/ ADDRESS:${add}/ TEL:${phone}`;
+    localStorage.setItem("order",info);
+})
+}
+
+
+
+
+
+
+
+
+
+
+
+
